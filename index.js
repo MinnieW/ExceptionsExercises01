8236ab4e890c2199642f1e2c14b297ecd73ada91
MinnieW <84355273+MinnieW@users.noreply.github.com>
2021-07-05 15:32:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+
+// Your function should return the result of numerator / denominator.
+
+// However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
+
+// Code your divide function here:
+
+function divide(numerator,denominator){
+  if (denominator === 0){
+    throw Error("You can't divide by 0")
+  } else{  
+  return numerator/denominator;}
+}
+
+console.log(divide(4,3))
